862536f6e589372c9832cba52b5de884add6d1be
HoustonNP <92217081+HoustonNP@users.noreply.github.com>
2022-04-07 10:34:28 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const nav = document.querySelector('nav');
 const navHeight = nav.offsetHeight;
+let navTop = () => nav.getBoundingClientRect().top;
 let scrollPosition = () => window.pageYOffset;
 let lastScrollDown = 0;
 let lastScrollUp = 0;
@@ -8,16 +9,17 @@ let lastScroll = 0;
 window.addEventListener('scroll', () => {
 
 	if (lastScroll < scrollPosition() &&
-		lastScrollUp + navHeight < lastScrollDown) {
+		navTop() == 0) {
 		nav.style.cssText = `top: ${lastScrollUp}px;`
 	}
+
 	if (lastScroll > scrollPosition() &&
-		lastScrollDown - navHeight > lastScrollUp) {
+		navTop() + navHeight < 0) {
 		nav.style.cssText = `top: ${lastScrollDown - navHeight}px;`
 	}
+
 	if (lastScroll > scrollPosition() &&
-		lastScrollDown !== lastScroll &&
-		lastScrollDown - navHeight >= scrollPosition()) {
+		navTop() > 0) {
 		nav.style.cssText = `position: fixed;`
 	}
 
@@ -27,5 +29,5 @@ window.addEventListener('scroll', () => {
 		lastScrollUp = scrollPosition()
 	}
 
-	lastScroll = scrollPosition()
+	lastScroll = scrollPosition();
 })
